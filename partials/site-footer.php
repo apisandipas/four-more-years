f861cc04909eac64c355b160b4b4f8e8b54d71dd
Bryan Paronto <bparonto@gmail.com>
2014-08-26 16:38:45 -0500

    adds new fb logo for sharing

--- partials/site-footer.php
+++ partials/site-footer.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
         
-            <div class="social col-md-3 col-md-offset-3">
+            <div class="social">
                 <ul>
                     <li><a href="<?php echo esc_attr( get_field('facebook_url', 'option') ); ?>" target="_blank" class="facebook"></a></li>
                     <li><a href="<?php echo esc_attr( get_field('instagram_url', 'option') ); ?>" target="_blank" class="instagram"></a></li>
@@ -11,7 +11,7 @@
                 </ul>
             </div>
 
-            <div class="copy col-md-3">
+            <div class="copy">
                 &copy; <?php echo sa_copyright_range(); ?> <?php bloginfo( 'name' ); ?> 
             </div>
 
